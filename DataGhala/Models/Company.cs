2535694519ecc88ee712da56df1671f322607296
ClaudeNambwaya <nambwaya92@gmail.com>
2022-02-18 15:21:09 +0300

    Add project files.

---
+++ DataGhala/Models/Company.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DataGhala.Models
+{
+    public class Company
+    {
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Publication { get; set; }
+        public string Author { get; set; }
+        public string Publisher { get; set; }
+        public string PublicationNumber { get; set; }
+        public string Source { get; set; }
+    }
+}
